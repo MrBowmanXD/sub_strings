723625842738b52256e2382e76c84af8b75ad68d
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2022-03-31 23:05:44 +0100

    Functional version of the method

---
+++ sub_strings.rb
@@ -0,0 +1,25 @@
+def sub_strings(compare, strings)
+  
+  words_array = []
+  words_hash = {}
+  strings.each do | string |
+    if compare.include?(' ')
+      
+      words_array.push(string.split)
+      words_array.each do | element|
+        if element.include?(string)
+          words_hash[string] = 1 
+        end
+      end
+    else
+      if compare.include?(string)
+        words_hash[string] = 1
+      end
+    end
+  end
+  words_hash
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+p sub_strings("below", dictionary)
+p sub_strings("Howdy partner, sit down! How's it going?", dictionary)
