13d74aa3bbeaf12a8e12b26169e09215e952dd6c
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2023-04-25 20:54:53 +0100

    Near possible solution

--- lib/sub_strings.rb
+++ lib/sub_strings.rb
@@ -2,7 +2,7 @@ class SubString
   def sub_string(string, dictionary)
     sub_string = {}
     dictionary.each do |word|
-      sub_string[word.downcase] = dictionary.count(word.downcase) if string.downcase.include?(word.downcase)
+      sub_string[word.downcase] = dictionary.count(word.downcase) if string.include?(word.downcase)
     end
     sub_string
   end
