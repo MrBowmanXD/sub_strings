939eb6815c100e1694be0b6fcfa8fdb0ae2be906
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2023-04-25 19:31:15 +0100

    First example is done correctly

---
+++ spec/sub_strings_spec.rb
@@ -0,0 +1,17 @@
+require './lib/sub_strings'
+
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+
+describe SubString do
+  describe '#sub_string' do
+    it 'return in a hash the subStrings (simple example)' do
+      str = SubString.new
+      expect(str.sub_string('below', dictionary)).to eql({ 'below' => 1, 'low' => 1 })
+    end
+
+    it 'return in a hash the subStrings (difficult example)' do
+      str = SubString.new
+      expect(str.sub_string("Howdy partner, sit down! How's it going?", dictionary)).to eql( { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 })
+    end
+  end
+end
